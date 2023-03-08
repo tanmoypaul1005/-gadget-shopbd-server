b543c3938337b513be8a11a3dd16c475002bf2e3
Tanmoy paul <84624966+tanmoypaul1005@users.noreply.github.com>
2023-03-08 15:13:48 +0600

    review product add

--- src/Route/ProductRoute.js
+++ src/Route/ProductRoute.js
@@ -1,15 +1,20 @@
 
 const app = require("express");
-const { AddProduct , getProductDetails , deleteProduct , editProduct} = require("../Controller/ProductController");
+const { AddProduct , getProductDetails , deleteProduct , editProduct, getProduct, addReview} = require("../Controller/ProductController");
 
 const router = app.Router();
 
 router.post('/product/add',AddProduct);
+router.get('/product',getProduct);
+
+
 router.get('/product/:id',getProductDetails);
 // router.post('/product/admin/get',GetProductAdmin);
 router.post('/product/delete',deleteProduct);
 // router.post('/product/id',ProductDetail);
 router.post('/product/edit',editProduct);
-// router.get('/product/filter/:category',GetProductsFilterByCategory)
+// router.get('/product/filter/:category',GetProductsFilterByCategory);
+
+router.post('product/reviews/add',addReview);
 
 module.exports = router;
