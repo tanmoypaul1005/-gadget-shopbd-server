10b2e7eed3640f891c49b1d45be4cf855eb51bf4
Tanmoy paul <84624966+tanmoypaul1005@users.noreply.github.com>
2023-03-10 13:00:30 +0600

    ok

--- src/Controller/OrderController.js
+++ src/Controller/OrderController.js
@@ -2,54 +2,52 @@ const Cart = require("../Models/Cart");
 const Order = require("../Models/Order");
 
 module.exports.addOrder = (req, res) => {
-    Cart.deleteOne({ user: req.body.user }).exec((error, result) => {
-      if (error) return res.status(400).json({ error });
-      if (result) {
-        req.body.user = req.body.user;
-        req.body.orderStatus = [
-          {
-            type: "ordered",
-            date: new Date(),
-            isCompleted: true,
-          },
+  Cart.deleteOne({ user: req.body.user }).exec((error, result) => {
+    if (error) return res.status(400).json({ error, success: false });
+    if (result) {
+      req.body.user = req.body.user;
+      req.body.orderStatus = [
+        {
+          type: "ordered",
+          date: new Date(),
+          isCompleted: true,
+        },
 
-          {
-            type: "packed",
-            isCompleted: false,
-          },
+        {
+          type: "packed",
+          isCompleted: false,
+        },
 
-          {
-            type: "shipped",
-            isCompleted: false,
-          },
+        {
+          type: "shipped",
+          isCompleted: false,
+        },
 
-          {
-            type: "delivered",
-            isCompleted: false,
-          },
+        {
+          type: "delivered",
+          isCompleted: false,
+        },
 
-        ];
-        const order = new Order(req.body);
-        order.save((error, order) => {
-          if (error) return res.status(400).json({ error });
-          if (order) {
-            res.status(201).json({ order });
-          }
-        });
-      }
-    });
-  };
-  
-
-
-  module.exports.getOrders = (req, res) => {
-    Order.find({ user: req.body.user })
-      .select("_id paymentStatus paymentType orderStatus items")
-      .populate("items.productId", "_id name productPicture")
-      .exec((error, orders) => {
-        if (error) return res.status(400).json({ error });
-        if (orders) {
-          res.status(200).json({ orders });
+      ];
+      const order = new Order(req.body);
+      order.save((error, data) => {
+        if (error) return res.status(500).json({ error,success:false });
+        if (data) {
+          res.status(200).json({ "message":"Your Order Add successfully",data ,success:true});
         }
       });
-  };
+    }
+  });
+};
+
+module.exports.getOrders = (req, res) => {
+  Order.find({ user: req.body.user })
+    .select("_id paymentStatus paymentType orderStatus items")
+    .populate("items.productId", "_id name productPicture")
+    .exec((error, data) => {
+      if (error) return res.status(400).json({ error,success: false  });
+      if (data) {
+        res.status(200).json({data ,success: true});
+      }
+    });
+};
--- src/Models/Order.js
+++ src/Models/Order.js
@@ -1,39 +1,37 @@
 const mongoose = require("mongoose");
+
 const OrderSchema = new mongoose.Schema(
   {
     user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+
     Address: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Address",
       required: true,
     },
+
     totalAmount: { type: Number, required: true },
+
     items: [
       {
-        productId: {
-          type: mongoose.Schema.Types.ObjectId,
-          ref: "Product",
-        },
-        payablePrice: {
-          type: Number,
-          required: true,
-        },
-        purchasedQty: {
-          type: Number,
-          required: true,
-        },
+        productId: {type: mongoose.Schema.Types.ObjectId,ref: "Product"},
+        payablePrice: {type: Number,required: true},
+        purchasedQty: {type: Number,required: true},
       },
     ],
+
     paymentStatus: {
       type: String,
       enum: ["pending", "completed", "cancelled", "refund"],
       required: true,
     },
+
     paymentType: {
       type: String,
       enum: ["cash", "card","bkash"],
       required: true,
     },
+
     orderStatus: [
       {
         type: {
@@ -41,13 +39,8 @@ const OrderSchema = new mongoose.Schema(
           enum: ["ordered", "packed", "shipped", "delivered"],
           default: "ordered",
         },
-        date: {
-          type: Date,
-        },
-        isCompleted: {
-          type: Boolean,
-          default: false,
-        },
+        date: {type: Date,},
+        isCompleted: {type: Boolean, default: false,},
       },
     ],
   },
